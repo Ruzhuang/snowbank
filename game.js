dcfabad06419dbbe0a9b83ec297b2fcbab84544e
ruzhuang <ruzhuang@ucsd.edu>
2021-10-10 18:39:09 -0700

    fixed size grid basic feature complete

--- game.js
+++ game.js
@@ -2,7 +2,7 @@ var width = 9
 var height = 9
 var allCells;
 var leftTurn = true
-const board = document.getElementById("page")
+const board = document.getElementById("board")
 
 startGame()
 
@@ -31,47 +31,125 @@ function startGame() {
             if (i % 2 == 0 && j % 2 == 0) {
                 allCells[i][j] = document.createElement("div")
                 allCells[i][j].classList.add("cell", "green")
-                allCells[i][j].addEventListener("mouseover", function() {
-                    handleOver(this, i, j)
-                })
-                allCells[i][j].addEventListener("mouseout", function() {
-                    handleOut(this, i, j)
-                })
 
             } else if (i % 2 == 0 && j % 2 == 1) {
                 allCells[i][j] = document.createElement("div")
                 allCells[i][j].classList.add("cell", "red")
-                allCells[i][j].addEventListener("mouseover", function() {
-                    handleOver(this, i, j)
-                })
-                allCells[i][j].addEventListener("mouseout", function() {
-                    handleOut(this, i, j)
-                })
+
             } else if (j % 2 == 0) {
                 allCells[i][j] = document.createElement("div")
                 allCells[i][j].classList.add("cell", "blue")
-                allCells[i][j].addEventListener("mouseover", function() {
-                    handleOver(this, i, j)
-                })
-                allCells[i][j].addEventListener("mouseout", function() {
-                    handleOut(this, i, j)
-                })
             } else if (j % 2 == 1) {
                 allCells[i][j] = document.createElement("div")
                 allCells[i][j].classList.add("cell")
             }
+            allCells[i][j].addEventListener("mouseover", function() {
+                handleOver(this, i, j)
+            })
+            allCells[i][j].addEventListener("mouseout", function() {
+                handleOut(this, i, j)
+            })
+            allCells[i][j].addEventListener("click", function() {
+                handleClick(this, i, j)
+            }, { once: true })
             board.appendChild(allCells[i][j])
         }
     }
 }
 
-function handleOver(e, i, j) {
-    e.style.opacity = "0.2"
+function handleClick(e, i, j) {
+    console.log(leftTurn)
+    if (e.classList.contains("green")) {
+        e.classList.remove("green", "red", "blue", "hover")
+        if (i > 0) {
+            allCells[i - 1][j].classList.remove("green", "red", "blue", "hover")
+        }
+        if (i < height - 1) {
+            allCells[i + 1][j].classList.remove("green", "red", "blue", "hover")
+        }
+        if (j > 0) {
+            allCells[i][j - 1].classList.remove("green", "red", "blue", "hover")
+        }
+        if (j < width - 1) {
+            allCells[i][j + 1].classList.remove("green", "red", "blue", "hover")
+        }
+    } else if (e.classList.contains("red")) {
+        if (leftTurn) {
+            e.classList.remove("green", "red", "blue", "hover")
+        }
+
+    } else if (e.classList.contains("blue")) {
+        if (!leftTurn) {
+            console.log("here")
+            e.classList.remove("green", "red", "blue", "hover")
+        }
+    } else {
+        return
+    }
+    checkWins()
+    leftTurn = !leftTurn
 
 }
 
+function checkWins() {
+    for (let i = 0; i < height; i++) {
+        for (let j = 0; j < width; j++) {
+            if (allCells[i][j].classList.contains("red") ||
+                allCells[i][j].classList.contains("green") ||
+                allCells[i][j].classList.contains("blue")) {
+                return
+            }
+        }
+    }
+    if (leftTurn) {
+        board.innerHTML = "Left Wins!"
+    } else {
+        board.innerHTML = "Right Wins!"
+
+    }
+}
+
+function handleOver(e, i, j) {
+    if (e.classList.contains("green")) {
+        e.classList.add("hover")
+        if (i > 0) {
+            allCells[i - 1][j].classList.add("hover")
+        }
+        if (i < height - 1) {
+            allCells[i + 1][j].classList.add("hover")
+        }
+        if (j > 0) {
+            allCells[i][j - 1].classList.add("hover")
+        }
+        if (j < width - 1) {
+            allCells[i][j + 1].classList.add("hover")
+        }
+    } else if (e.classList.contains("red")) {
+        if (leftTurn) {
+            e.classList.add("hover")
+        }
+
+    } else if (e.classList.contains("blue")) {
+        if (!leftTurn) {
+            e.classList.add("hover")
+        }
+    }
+}
+
 function handleOut(e, i, j) {
-    e.style.opacity = "1"
+    e.classList.remove("hover")
+    if (i > 0) {
+        allCells[i - 1][j].classList.remove("hover")
+    }
+    if (i < height - 1) {
+        allCells[i + 1][j].classList.remove("hover")
+    }
+    if (j > 0) {
+        allCells[i][j - 1].classList.remove("hover")
+    }
+    if (j < width - 1) {
+        allCells[i][j + 1].classList.remove("hover")
+    }
 }
 
 // const cell = document.getElementById("cell")
